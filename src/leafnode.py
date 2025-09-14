2d2cc99e1dc4a5752f360788373e5d1a9a2f8d39
Markus Carlbark <markus.carlbark@gmail.com>
2025-09-14 22:17:30 +0200

    Added leafnode subclass

---
+++ src/leafnode.py
@@ -0,0 +1,20 @@
+from htmlnode import HTMLNode
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag,value, None, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("All nodes leaf nodes must have a value")
+        if self.props == None:
+            if self.tag == None:
+                return self.value
+            else:
+                 return "<"+self.tag+">"+self.value+"</"+self.tag+">"
+        else: 
+            return "<"+self.tag+self.props_to_html()+">"+self.value+"</"+self.tag+">"
+
+
+
