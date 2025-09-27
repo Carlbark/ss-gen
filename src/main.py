61953e0e32aee48e1c3586fafa7160c5db117b01
Markus Carlbark <markus.carlbark@gmail.com>
2025-09-27 22:09:17 +0200

    Expanded main.py to iintialize a public directory as a copy of a source dir (static).

--- src/main.py
+++ src/main.py
@@ -1,9 +1,30 @@
+import os
+import shutil   
 from textnode import TextNode, TextType
 
 
 def main():
-    print("Hello world")
-    textnode = TextNode("This is some anchor text", TextType.LINK, "https://example.com")
-    print(textnode)
+    print("Copying static to public...")
+    recursive_copy("static", "public")
+
+
+def recursive_copy(src="static/", dst="public/"):
+    # Delete all content in the destination directory
+    if os.path.exists(dst):
+        print(f"Deleting existing content in {dst}...")
+        shutil.rmtree(dst)
+    print(f"Creating directory {dst}")
+    os.makedirs(dst, exist_ok=True)
+
+    # Recursively copy all content from src to dst
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            print(f"Recursive copy {s} to {d}...")
+            recursive_copy(s, d)
+        else:
+            print(f"Copying file from {s} to {d}...")
+            shutil.copy2(s, d)
 
 main()
